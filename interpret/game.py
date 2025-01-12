c2a7ba3fb902ff50f8e0bf810da8c3fe38f23b1f
Finn Eyles <feyles@icloud.com>
2025-01-12 23:51:00 +0000

    added Movement.pretty

--- interpret/game.py
+++ interpret/game.py
@@ -47,6 +47,9 @@ class Movement:
             is_out = False
 
         return Movement(runner, start_base, end_base, is_out)
+    
+    def pretty(self) -> str:
+        return f"{self.runner} {self.start_base} -> {self.end_base} {'[out]' if self.is_out else ''}"
 
 
 class PlayType(Enum):
